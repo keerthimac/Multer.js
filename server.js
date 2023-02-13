0b216dfaf203d3d374539e59fc446f656c335403
keerthi <keerthi.mac@gmail.com>
2023-02-13 18:25:26 +0530

    structure multer

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const colors = require("colors");
 const PORT = 5000;
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 
@@ -9,6 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Multer test" });
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.send("image Uploaded");
 });
 
 app.listen(PORT, () => {
